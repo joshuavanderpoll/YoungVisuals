c7fe561de7ce06f8c92a0457220947bf75f41cce
Joshua van der Poll <joshuavanderpoll@msn.com>
2019-09-18 14:08:03 +0200

    Added header

--- private/pages/home.php
+++ private/pages/home.php
@@ -1,4 +1,22 @@
 <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
 <body>
-	<h1>Font Test</h1>
+	<header>
+		<div class="header__overlay">
+			<div class="header__top">
+				<img src="public/img/logo_white.svg" alt="YoungVisual Logo">
+				<ul>
+					<li class="active">Home</li>
+					<li>Diensten</li>
+					<li>Over ons</li>
+					<li>Portfolio</li>
+					<li>Contact</li>
+				</ul>
+				<button>Offerte Aanvragen</button>
+			</div>
+			<div class="header__title">
+				<h1>Making brands visible.</h1>
+				<h2>We zijn gespecialiseerd in WordPress Development & Online Marketing</h2>
+			</div>
+		</div>
+	</header>
 </body>
