8cfe7cff985e1094a75b57803477465b9cce6fb3
Joshua van der Poll <joshuavanderpoll@msn.com>
2019-09-20 13:47:48 +0200

    Fixed menu and added contact

--- private/pages/diensten.php
+++ private/pages/diensten.php
@@ -3,14 +3,14 @@
 		<div class="header__overlay">
 			<div class="header__top">
 				<img src="public/img/logo_white.svg" alt="YoungVisual Logo">
-				<ul>
-					<li class="active">Home</li>
-					<li>Diensten</li>
-					<li>Over ons</li>
-					<li>Portfolio</li>
-					<li>Contact</li>
-				</ul>
-				<button>Offerte Aanvragen</button>
+                <ul>
+                    <li onclick="location.href = '/'">Home</li>
+                    <li onclick="location.href = 'diensten'" class="active">Diensten</li>
+                    <li onclick="location.href = 'over-ons'">Over ons</li>
+                    <li onclick="location.href = 'portfolio'">Portfolio</li>
+                    <li onclick="location.href = 'contact'">Contact</li>
+                </ul>
+				<!-- <button>Offerte Aanvragen</button> -->
 			</div>
 			<div class="header__title">
 				<h1>Making brands visible.</h1>
